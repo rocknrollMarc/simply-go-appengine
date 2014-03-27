574bc3bae97e72858de28e675c6125ba470759e5
Damon P. Cortesi <d.lifehacker@gmail.com>
2014-03-27 14:32:55 -0700

    Add bootstrap and use a template for index page

--- controllers/index.go
+++ controllers/index.go
@@ -2,13 +2,27 @@ package controllers
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", indexPage)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+// START FUNCTIONS FOR INDEX PAGE
+
+var indexTemplate = template.Must(template.New("index.html").ParseFiles(
+	"views/index.html",
+))
+
+type IndexPageVariables struct {
 }
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	if err := indexTemplate.Execute(w, IndexPageVariables{}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// END FUNCTIONS FOR INDEX PAGE
